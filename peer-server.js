935ddc8e1bfbb5b430a7612fe85448d9519f541d
vishal <vishal.k@simplifycv.com>
2021-09-03 21:09:05 +0530

    first committ

--- peer-server.js
+++ peer-server.js
@@ -3,6 +3,7 @@ var PeerServer = require('peer').PeerServer;
 
 var server = PeerServer({
     port: 9000,
+    host: '157.245.101.140',
     path: '/peerjs',
     ssl: {
         key: fs.readFileSync('key.pem', 'utf8'),
